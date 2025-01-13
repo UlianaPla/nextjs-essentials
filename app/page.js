e7fd3502bba1311390d439dffc114b13ab579d73
Ulia <lyanusia@gmail.com>
2025-01-13 13:34:31 +0100

    components usage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
 
-export default function Home() {
-  console.log('Executing/...');
+import Header from '@/components/header';
 
+export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p> <Link href="/about">About Us</Link></p>
 
